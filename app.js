8e4aba6d0b918a3c535dbd6b254ce58e59738341
YohanesMarchel <aclprcjt@gmail.com>
2023-08-29 22:06:17 +0800

    redesin fillter api

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
 const containerMenus = document.querySelector(".menus .data-menus");
 
-// fetch data dari api/path
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    // JSON to object
-    let dataMenus = JSON.parse(this.responseText);
-    let menu = dataMenus.menu;
+const viewAllMenus = () => {
+  // fetch data dari api/path
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // JSON to object
+      let dataMenus = JSON.parse(this.responseText);
+      let menu = dataMenus.menu;
 
-    // tampilkan data
-    let output = "";
-    menu.forEach((i) => {
-      output += `
+      // tampilkan data
+      let output = "";
+      menu.forEach((i) => {
+        output += `
       <div
-      class="card w-full h-max lg:w-1/5 bg-white shadow-lg overflow-hidden"
+      class="card w-full h-max lg:w-1/5 mr-4 mb-4 bg-white shadow-lg overflow-hidden"
     >
       <img src="data/img/pizza/${i.gambar}" class="w-full" alt="" />
       <div class="dec p-5">
-        <h1 class="text-2xl font-semibold">${i.nama}</h1>
-        <p class="py-2 text-base">
+        <h1 class="text-lg font-semibold">${i.nama}</h1>
+        <p class="py-2 text-sm">
         ${i.deskripsi}
         </p>
-        <p class="text-xl font-semibold">Rp. ${i.harga} ,-</p>
+        <p class="text-lg font-semibold">Rp. ${i.harga} ,-</p>
         <button
-          class="mt-3 rounded-md font-normal bg-secondary p-2 text-white"
+          class="mt-3 rounded-md font-normal bg-secondary p-2 text-white text-sm"
         >
           Beli sekarang
         </button>
       </div>
     </div>
       `;
-    });
+      });
 
-    console.log(menu);
-    containerMenus.innerHTML = output;
-  }
+      console.log(menu);
+      containerMenus.innerHTML = output;
+    }
+  };
+  xhr.open("GET", "./data/pizza.json", true);
+  xhr.send();
 };
-xhr.open("GET", "./data/pizza.json", true);
-xhr.send();
 
-
+viewAllMenus();
 
 const menuItems = document.querySelector(".items");
 // menu active fungtion
@@ -53,10 +55,51 @@ window.onload = () => {
     // fillter menu by category
     let category = e.target.innerText;
     document.querySelector(".menus .title").innerHTML = category;
+
+    if (category == "All Menus") {
+      viewAllMenus();
+      return;
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        // JSON to object
+        let dataMenus = JSON.parse(this.responseText);
+        let menu = dataMenus.menu;
+
+        // tampilkan data
+        let content = "";
+        menu.forEach((i) => {
+          if (i.kategori == category.toLowerCase())
+            content += `
+      <div
+      class="card w-full h-max lg:w-1/5 mr-4 mb-4 bg-white shadow-lg overflow-hidden"
+    >
+      <img src="data/img/pizza/${i.gambar}" class="w-full" alt="" />
+      <div class="dec p-5">
+        <h1 class="text-lg font-semibold">${i.nama}</h1>
+        <p class="py-2 text-sm">
+        ${i.deskripsi}
+        </p>
+        <p class="text-lg font-semibold">Rp. ${i.harga} ,-</p>
+        <button
+          class="mt-3 rounded-md font-normal bg-secondary p-2 text-white text-sm"
+        >
+          Beli sekarang
+        </button>
+      </div>
+    </div>
+      `;
+        });
+
+        containerMenus.innerHTML = content;
+      }
+    };
+    xhr.open("GET", "./data/pizza.json", true);
+    xhr.send();
   });
 };
-
-
 
 // menu bars
 const bars = document.querySelector(".bars");
